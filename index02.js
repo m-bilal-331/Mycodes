8de1ed0ea16c88ea0aa059ede77bff84b069f51d
m-bilal-331 <rao.mbilal331@gmail.com>
2023-10-16 18:04:28 +0500

    Intro of Hoisting with function and class

---
+++ index02.js
@@ -0,0 +1,19 @@
+//Fucntion Declaration Syntrax are hoisted
+
+function sayHello () {
+
+}
+
+//Function Expression Syntax are not Hoisted
+
+const sayGoodbye = function () {};
+
+// Class declaration are not hoisted
+
+class Circle {
+
+}
+
+//Class Expression are not hoisted
+
+const square = class {};
